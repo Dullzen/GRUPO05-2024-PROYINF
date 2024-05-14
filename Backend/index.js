d400ff6f5abd11e29e80225e0c988c0823c5bc35
Elias Valle <91441332+Maxonman@users.noreply.github.com>
2024-05-13 21:10:21 -0400

    Delete Backend directory

--- Backend/index.js
+++
@@ -1,5 +0,0 @@
-import database from "./src/database.js";
-import server from "./src/server.js";
-server.listen(process.env.PORT, () =>
-	console.log(`Backend corriendo en http://localhost:${process.env.PORT}`)
-);
